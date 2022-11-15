92e7e80c403f014fd0a2e06312e2d9dde3dde2bf
trrakshitha-reddy <117572494+trrakshitha-reddy@users.noreply.github.com>
2022-11-15 20:57:51 +0530

    Add files via upload

--- sqrt.py
+++ sqrt.py
@@ -1,17 +1,36 @@
+# sumsqr=0
+# sumcube=0
+# print("please enter the 10 numbers")
+# for i in range(1,11):
+#     num=int(input("number%d= "%i))
+#     square=i*i
+#     sumsqr=sumsqr+square
+#     avgsqr=sumsqr/10
+#     cube=i*i*i
+#     sumcube=sumcube+cube
+#     avgcube=sumcube/10
+# print("the square of numbers= ",square)
+# print("the sum of square of numbers= ",sumsqr)
+# print("the average of squared numbers= ",avgsqr)
+# print("the cube of numbers ",cube)
+# print("sum of cube of numbers= ",sumcube)
+# print("average of cubed numbers= ",avgcube)
+i=0
 sumsqr=0
 sumcube=0
-print("please enter the 10 numbers")
-for i in range(1,11):
-    num=int(input("number%d= "))
+n=10
+print(f"number square cube")
+while i<n:
+    i=int(input("number %d= "%i))
     square=i*i
-    sumsqr=sumsqr+square
-    avgsqr=sumsqr/10
     cube=i*i*i
+    sumsqr=sumsqr+square
     sumcube=sumcube+cube
+    avgsqr=sumsqr/10
     avgcube=sumcube/10
-print("the square of numbers= ",square)
-print("the sum of square of numbers= ",sumsqr)
-print("the average of squared numbers= ",avgsqr)
-print("the cube of numbers ",cube)
-print("sum of cube of numbers= ",sumcube)
-print("average of cubed numbers= ",avgcube)
+    print(f" {square}    {cube}")
+    print("sum of square of 10 numbers= ",sumsqr)
+    print("sum of cube of 10 numbers= ",sumcube)
+    print("average of square= ",avgsqr)
+    print("average of cube=",avgcube)
+
